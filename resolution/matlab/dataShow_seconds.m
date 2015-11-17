59cff033d8958bba61f342fe75f9a85ac8123cc0
david-alejo <churr.te@gmail.com>
2015-11-17 13:44:34 +0100

    Data Show seconds, first version

--- resolution/matlab/dataShow_seconds.m
+++ resolution/matlab/dataShow_seconds.m
@@ -17,18 +17,18 @@ color=['b' 'm' 'g' 'k' 'y' 'r' 'b--' 'm--' 'g--' 'k--' 'y--' 'r--'];
 % The output will be:
 % cost{method}{experiment}{t}
 
-t_vector = [0:delta_t:final_time];
+t_vector = [delta_t:delta_t:final_time];
 
 % First get the maximum with each method
-maxs = -(ones(length(data{curr_method}{n}{2}), 1)* inf);
-mins = ones(length(data{curr_method}{n}{2}), 1)*inf;
+maxs = -(ones(length(data{1}{n}{2}), 1)* inf);
+mins = ones(length(data{1}{n}{2}), 1)*inf;
 for curr_method=1:length(data)
   for m=1:length(data{curr_method}{n}{2})
-    for it=1:last_iteration
-      aux2getNormMedianCost{curr_method}(m,it)=data{curr_method}{n}{1}{m}(it);
+    for it=1:length(data{curr_method}{n}{2}{m})
+      aux{curr_method}(m,it)=data{curr_method}{n}{1}{m}(it);
     end
-    maxs(m) = max([maxs(m) max(aux2getNormMedianCost{curr_method}(m, :))]);
-    mins(m) = min([mins(m) min(aux2getNormMedianCost{curr_method}(m, :))]);
+    maxs(m) = max([maxs(m) max(aux{curr_method}(m, :))]);
+    mins(m) = min([mins(m) min(aux{curr_method}(m, :))]);
   end
 end
 diffs = maxs - mins ;
@@ -37,112 +37,91 @@ b = -mins./diffs;
 
 % get the cost over the time (starting from the maximum cost in each case)
 figure;
+max_cost = max(maxs);
 aux2getMedian = cell(length(data), 1);
 for curr_method=1:length(data)
   for m=1:length(data{curr_method}{n}{2}) % For each experiment
     cont_t = 1;
     last_cost = max_cost;
-    curr_time = data{curr_method}{n}{2}(1)
+    curr_time = data{curr_method}{n}{2}{m}(1);
     last_time = 0;
     cont_it = 1;
     for t = delta_t:delta_t:final_time
       % First get the first value above
-      while (data{curr_method}{n}{2}(cont_it) < t)
-        last_cost = data{curr_method}{n}{1}(cont_it);
-        last_time = data{curr_method}{n}{2}(cont_it);
+      while (cont_it <= length(data{curr_method}{n}{2}{m}) && data{curr_method}{n}{2}{m}(cont_it) < t)
+        last_cost = data{curr_method}{n}{1}{m}(cont_it);
+        last_time = data{curr_method}{n}{2}{m}(cont_it);
         cont_it = cont_it + 1;
       end
       % Get the time and cost of the current data used
-      curr_time = data{curr_method}{n}{2}(cont_it);
-      curr_cost = data{curr_method}{n}{1}(cont_it);
-      % Interpolation
-      cost{curr_method}(m,cont) = last_cost + (curr_cost - last_cost)/(curr_time - last_time) * (t - last_time);
+      if (cont_it <= length(data{curr_method}{n}{2}{m}))
+	curr_time = data{curr_method}{n}{2}{m}(cont_it);
+	curr_cost = data{curr_method}{n}{1}{m}(cont_it);
+	% Interpolation
+	cost{curr_method}(m,cont_t) = last_cost + (curr_cost - last_cost)/(curr_time - last_time) * (t - last_time);
+	cont_t = cont_t + 1;
+	
+      end
     end
   end
   median_cost{curr_method}=median(cost{curr_method});
  
   
-  plot(time_vector, median_time{curr_method}{2}, color(curr_method), 'lineWidth', 3);
+  plot(t_vector, median_cost{curr_method}, color(curr_method), 'lineWidth', 3);
   hold on;
 end
 
-setLabelStyle('Number of iterations', 'Median of the cost(s)');
+setLabelStyle('Execution time (s)', 'Median of the cost');
 legend_handle=legend(text);
-set(legend_handle,'fontsize',18, 'location', 'northwest');
+set(legend_handle,'fontsize',28, 'location', 'northeast');
+axis([0 final_time 0 max_cost]);
+hold off;
+
+ 
+%  % Normalized cost through successive iterations [parametrized with n_uavs]
+figure;
+for curr_method=1:length(data)
+  for m=1:length(data{curr_method}{n}{1})
+    for cont_t=1:length(t_vector)
+      if isnan(a(m)) == 0
+	norm_cost{curr_method}(m,cont_t)=cost{curr_method}(m,cont_t)*a(m) + b(m);
+	if isnan(norm_cost{curr_method}(m, cont_t)) == 1
+	  norm_cost{curr_method}(m, cont_t) = 0;
+	end
+      else 
+	norm_cost{curr_method}(m, cont_t) = 0;
+      end
+    end
+  end
+  median_norm_cost{curr_method}=median(norm_cost{curr_method});
+  plot(t_vector, median_norm_cost{curr_method}, color(curr_method), 'lineWidth', 3);
+  
+  hold all;
+end %for curr_method
+%    
+%  
+line([0 final_time], [0.1 0.1], 'Color', 'k', 'LineWidth', 2, 'LineStyle', '--');
+axis([0 final_time 0 1]);
+setLabelStyle('Execution time (s)', 'Median of the normalized cost');
+legend_handle=legend(text);
+set(legend_handle,'fontsize',18);
 hold off;
 
 
-% Median of the minimum cost against iterations [parametrized with n_uavs]
-%  figure;
-%  aux2getNormMedianCost = cell(length(text), 1);
-%  for curr_method=1:length(data)
-%      aux2getNormMedianCost{curr_method}=zeros(length(data{curr_method}{n}{1}),last_iteration);
-%      for m=1:length(data{curr_method}{n}{1})
-%          for it=1:last_iteration
-%              aux2getNormMedianCost{curr_method}(m,it)=data{curr_method}{n}{1}{m}(it);
-%          end
-%          
-%      end
-%      fig2_data{curr_method}{2}=median(aux2getNormMedianCost{curr_method});
-%      fig2_data{curr_method}{1}=1:length(fig2_data{curr_method}{2});
-%      plot(fig2_data{curr_method}{1}, fig2_data{curr_method}{2}, color(curr_method), 'lineWidth', 3);
-%      hold on;
-%  end
-%  setLabelStyle('Number of iterations', 'Median of the minimum cost (m)');
-%  [legend_handle]=legend(text);
-%  set(legend_handle,'fontsize',18);
-%  hold off;
-%  
-%  % Normalized cost through successive iterations [parametrized with n_uavs]
-%  figure;
-%  
-%  
-%  
-%  
-%  for curr_method=1:length(data)
-%    for m=1:length(data{curr_method}{n}{1})
-%      for it=1:last_iteration
-%        if isnan(a(m)) == 0
-%  	aux2getNormMedianCost{curr_method}(m,it)=aux2getNormMedianCost{curr_method}(m,it)*a(m) + b(m);
-%  	if isnan(aux2getNormMedianCost{curr_method}(m,it)) == 1
-%  	  aux2getNormMedianCost{curr_method}(m,it) = 0;
-%  	end
-%        else 
-%  	aux2getNormMedianCost{curr_method}(m,it) = 0;
-%        end
-%      end
-%    end
-%    fig3_data{curr_method}{2}=median(aux2getNormMedianCost{curr_method});
-%    fig3_data{curr_method}{1}=1:length(fig3_data{curr_method}{2});
-%    plot(fig3_data{curr_method}{1}, fig3_data{curr_method}{2}, color(curr_method), 'lineWidth', 3);
-%    
-%    hold all;
-%  end %for curr_method
-%    
-%  
-%  line([1 it], [0.1 0.1], 'Color', 'k', 'LineWidth', 2, 'LineStyle', '--');
-%  setLabelStyle('Number of iterations', 'Median of the normalized cost');
-%  legend_handle=legend(text);
-%  set(legend_handle,'fontsize',18);
-%  hold off;
-%  
-%  
 %  % Anytime figures will use the data in figures 1 and 2
-%  figure;
-%  for curr_method=1:length(data)
-%    fig4_data{curr_method}{2}(1) = 0;
-%    fig4_data{curr_method}{1}(1) = 0;
-%    for i=1:last_iteration
-%      fig4_data{curr_method}{2}(i+1)=(1-fig3_data{curr_method}{2}(i))*100; % Conversion from C* to optimality
-%      fig4_data{curr_method}{1}(i+1)=median_time{curr_method}{2}(i); % The time in seconds
-%    end
-%    plot(fig4_data{curr_method}{1}, fig4_data{curr_method}{2}, color(curr_method), 'lineWidth', 3);
-%    hold all;
-%  end
-%  
-%  setLabelStyle('Execution time (s)', 'Optimality (%)');
-%  legend_handle=legend(text);
-%  set(legend_handle,'fontsize', 18);
-%  hold off;
-%  
-%  end
+figure;
+for curr_method=1:length(data)
+  for i=1:length(t_vector)
+    fig4_data{curr_method}(i)=(1-median_norm_cost{curr_method}(i))*100; % Conversion from C* to optimality
+  end
+  plot(t_vector, fig4_data{curr_method}, color(curr_method), 'lineWidth', 3);
+  hold all;
+end
+
+setLabelStyle('Execution time (s)', 'Optimality (%)');
+axis([0 final_time 0 100]);
+legend_handle=legend(text);
+set(legend_handle,'fontsize', 18);
+hold off;
+
+end
